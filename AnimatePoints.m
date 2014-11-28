f429a874dee90c366790fe4c6335948299cce6c4
hyprmeghan <hyprmeghan@gmail.com>
2014-11-28 15:47:57 +0800

    Graphs drawn
    Now only need Track Point and Animate Points. Will go through and draw
    a graph for every set of point with lines in between.

--- AnimatePoints.m
+++ AnimatePoints.m
@@ -1,11 +1,8 @@
-filepath = '/Users/meghan/Desktop/CinePlexbased videos/Rat Videos and CinePlex Data/Side View Data Text Files';
-filenames = {'20140723002_2','20140723009_2', '20140723010_2'};
+filepath = '/Users/meghan/Desktop/Testing';
+filenames = {'20141128001_2'};
 frameMat = [1 3 7 8 9 10; 1 3 6 9 11 13; 1  3 5 9 11 21];
+numVars = 3;
 
-%frameMat = [2 4 8 10 11 19; 2 4 7 12 14 17; 2 5 7 12 14 27];
-[aTime, aTheta] = LegAngle(filepath, filenames,frameMat);
-aTheta(1,:)
-%aTheta(10,:)
-%aTheta(19,:)
+TrackPoints(filepath, filenames,frameMat, numVars);
 
 'Next'
