4e05210be8b8a353ae24a72c83ac54c1030f3c96
hyprmeghan <hyprmeghan@gmail.com>
2014-12-09 14:33:19 +0800

    Improved navigation of data set
    Time navigation doesn’t work. Last plot doesn’t work, issues with
    length of theta

--- AnimatePoints.m
+++ AnimatePoints.m
@@ -6,3 +6,4 @@ numVars = 3;
 TrackPoints(filepath, filenames, numVars);
 
 'Next'
+ 
--- aequal.m
+++ aequal.m
@@ -2,7 +2,7 @@ function [ result ] = aequal( v1, v2 )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-if abs(v1 - v2) < 0.001
+if abs(v1 - v2) < 0.1/6
     result = true;
 else
     result = false;
